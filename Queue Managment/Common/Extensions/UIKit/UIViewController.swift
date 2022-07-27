4db39e61b830e43002fb8c089220f57527fd65b2
zuratvaradze <zuratvaradze999@gmail.com>
2022-07-27 14:23:15 +0400

    TextFieldDelegate

---
+++ Queue Managment/Common/Extensions/UIKit/UIViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController.swift
+//  Queue Managment
+//
+//  Created by Zura Tvaradze on 27.07.22.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
